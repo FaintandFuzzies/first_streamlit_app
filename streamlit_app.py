0fa04a4f39fa7cca7f795932a475528d0c15d3ca
FaintandFuzzies <32536506+FaintandFuzzies@users.noreply.github.com>
2022-08-03 12:40:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text(:muscle:'Omega 3 Blueberry Oatmeal')
-streamlit.text(:leaves:'Kale, Spinach & Rocket Smoothie')
-streamlit.text(:chicken:'Hard-Boiled Egg')
+streamlit.text(':muscle:Omega 3 Blueberry Oatmeal')
+streamlit.text('🍌🥭Kale, Spinach & Rocket Smoothie')
+streamlit.text(':chicken:Hard-Boiled Egg')
